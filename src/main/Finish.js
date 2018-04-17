6a806498eed543991422853e81ada9bb3f88926d
Madeline Wong <madelinegwong@outlook.com>
2018-04-17 11:12:49 -0400

    added a background image for my results container

--- src/main/Finish.js
+++ src/main/Finish.js
@@ -4,13 +4,13 @@ import React from 'react';
 const Finish = props => (
     <div> 
         <style>
-@import url('https://fonts.googleapis.com/css?family=Lobster');
+        @import url('https://fonts.googleapis.com/css?family=Cinzel');
 </style>
-        <h1 className="first">Great, now what type of finish are you looking for in your foundation?</h1>
-        <button onClick={e => props.onFinishType(e.target.textContent)}>Matte</button>
-        <button onClick={e => props.onFinishType(e.target.textContent)}>Dewy</button>
-        <button onClick={e => props.onFinishType(e.target.textContent)}>Radiant</button>
-        <button onClick={e => props.onFinishType(e.target.textContent)}>Smooth</button>
+        <button onClick={e => props.onFinishType(e.target.textContent)} className="vert-button--btn">Matte</button>
+        <button onClick={e => props.onFinishType(e.target.textContent)} className="vert-button--btn">Dewy</button>
+        <button onClick={e => props.onFinishType(e.target.textContent)} className="vert-button--btn">Radiant</button>
+        <button onClick={e => props.onFinishType(e.target.textContent)} className="vert-button--btn">Smooth</button>
+        <span className="vert-button--btn">Great, now what type of finish are you looking for in your foundation?</span>
         <button onClick={() => props.changePlace('Results')}>ready</button>
     </div> 
 )
--- src/main/SkinType.js
+++ src/main/SkinType.js
@@ -9,10 +9,10 @@ const SkinType = props => (
 <style>
 @import url('https://fonts.googleapis.com/css?family=Cinzel');
 </style>
-<div className="container"> Okay, let's get started. What type of skin do you have? </div>
-        <button onClick={e => props.onSkinType(e.target.textContent)} className="oily">oily</button>
-        <button onClick={e => props.onSkinType(e.target.textContent)} className="combo">combo</button>
-        <button onClick={e => props.onSkinType(e.target.textContent)} className="dry">dry</button>
+        <span className="vert-button--btn1"> Okay, let's get started. What type of skin do you have?</span>
+        <button onClick={e => props.onSkinType(e.target.textContent)} className="vert-button--btn">oily</button>
+        <button onClick={e => props.onSkinType(e.target.textContent)} className="vert-button--btn">combo</button>
+        <button onClick={e => props.onSkinType(e.target.textContent)} className="vert-button--btn">dry</button>
         <button className="button1" onClick={() => props.changePlace('finish')} className="next">next!</button>
     </div>
 )
